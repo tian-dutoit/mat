6eac6e206d25cd5300dae0940530a38a34626138
Tian du Toit <tian@hmsnz.com>
2018-09-21 11:20:38 +1200

    homepage setup with material ui

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Home from './Home'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <React.Fragment>
+      <CssBaseline />
+        <div className="App">
+          <Home />
+        </div>
+      </React.Fragment>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/Home.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import Button from '@material-ui/core/Button'
+import './Home.css'
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home-container">
+       <div className='header'></div>
+       <div className='content'>
+        <Button variant="contained" color="primary">
+          Home sweet home
+        </Button>
+       </div>
+      </div>
+    )
+  }
+}
+
+export default Home
