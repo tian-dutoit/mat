2ff3cc1c7796fb02cec1e2c7395532c248adcad2
Tian du Toit <tian@hmsnz.com>
2018-09-24 14:44:15 +1200

    Convert suggestions to array

--- src/Search.jsx
+++ src/Search.jsx
@@ -2,16 +2,7 @@ import React, { Component } from 'react'
 import Autosuggest from 'react-autosuggest'
 
 // Imagine you have a list of languages that you'd like to autosuggest.
-const languages = [
-  {
-    name: 'C',
-    year: 1972
-  },
-  {
-    name: 'Elm',
-    year: 2012
-  },
-]
+const languages = ['alpha', 'aa', 'alpha2', 'bravo', 'charlie', 'delta', 'echo', 'foxtrot', 'golf', 'hotel', 'india', 'juliett', 'kilo', 'lima', 'mike', 'november', 'oscar', 'pap', 'quebec', 'romeo', 'sierra', 'tango', 'uniform', 'victor', 'whiskey', 'x-ray', 'yankee', 'zulu']
 
 // Teach Autosuggest how to calculate suggestions for any given input value.
 const getSuggestions = value => {
@@ -19,25 +10,25 @@ const getSuggestions = value => {
   const inputLength = inputValue.length;
 
   return inputLength === 0 ? [] : languages.filter(lang =>
-    lang.name.toLowerCase().slice(0, inputLength) === inputValue
+    lang.toLowerCase().slice(0, inputLength) === inputValue
   )
 }
 
 // When suggestion is clicked, Autosuggest needs to populate the input
 // based on the clicked suggestion. Teach Autosuggest how to calculate the
 // input value for every given suggestion.
-const getSuggestionValue = suggestion => suggestion.name;
+const getSuggestionValue = suggestion => suggestion;
 
 // Use your imagination to render suggestions.
 const renderSuggestion = suggestion => (
   <div>
-    {suggestion.name}
+    {suggestion}
   </div>
 );
 
 class Search extends React.Component {
   constructor() {
-    super();
+    super()
 
     // Autosuggest is a controlled component.
     // This means that you need to provide an input value
